46b945d32d98cfce6647bfd172f8f455b5508ae5
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-01-06 20:19:39 +0100

    Added setup instructions for pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+Setup script for PyPi
+"""
+
+from distutils.core import setup
+setup(name='scrapy-mongodb',
+    version='0.1.0',
+    license='Apache License, Version 2.0',
+    description='Pipeline to MongoDB for Scrapy. Supports MongoDB replica sets',
+    author='Sebastian Dahlgren',
+    author_email='sebastian.dahlgren@gmail.com',
+    url='http://github.com/sebdah/scrapy-mongodb',
+    keywords="scrapy mongodb",
+    py_modules=['scrapy_mongodb'],
+    platforms=['Any'],
+    install_requires=['scrapy', 'pymongo'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
