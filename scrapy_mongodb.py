af4389c89c02a360bbdb21f75ceb1431366784b4
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-05 11:02:07 +0100

    Bumped version to 0.8.1

--- scrapy_mongodb.py
+++ scrapy_mongodb.py
@@ -30,7 +30,7 @@ from pymongo.read_preferences import ReadPreference
 from scrapy import log
 from scrapy.contrib.exporter import BaseItemExporter
 
-VERSION = '0.8.0'
+VERSION = '0.8.1'
 
 
 def not_set(string):
